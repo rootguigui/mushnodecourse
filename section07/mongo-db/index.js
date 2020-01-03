c177bea88512cd84950b12b985093fdf60c5771a
Guilherme Almeida <guilhermealmeida48@gmail.com>
2020-01-03 01:26:42 -0300

    90.Comparison Query Operators

--- section07/mongo-db/index.js
+++ section07/mongo-db/index.js
@@ -1,6 +1,39 @@
 const mongoose = require('mongoose');
 
-
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB....', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+const course = new Course({
+    name: 'Angular 2+ Course',
+    author: 'Mosh',
+    tags: [ 'Angularjs', 'frontend' ],
+    isPublished: true
+});
+
+async function createCourse() {
+    const result = await course.save();
+    console.log('course is saved...', result);
+}
+
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'Mosh' })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 , author: 1 });
+
+    console.log(courses);
+}
+
+getCourses();
+// createCourse();
